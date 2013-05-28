3f850a78ba01ccfa47a23f58d17d3e06b425d245
ivan rocca <ivan.loa@sead.ufscar.br>
2013-05-28 17:48:39 -0300

    primeira versão

---
+++ MathJong/scripts/models/model.js
@@ -0,0 +1,404 @@
+define(["./checagemLigacao2", "./tabelaDeDados"], function (checagem, tabela) 
+{
+	
+	var CUSTO_DA_DICA = 150,
+	PONTOS_POR_ACERTO = 50,
+	NIVEL_MAXIMO,
+	NUM_DE_LINHAS,
+	NUM_DE_COLUNAS,
+	matrizDePecas,
+	tempoRestante,
+	pontuacao = 0,
+	estadoDoJogo = "parado",//venceu, perdeu, impossivel, jogando, parado, pausado
+	gravidadeLigada,
+	nivel = 0,
+	cronometro;	
+	function gerarNovaMatriz()
+	{		
+		//Essa função é chamada no começo do jogo é gera aleatoriamente a matriz com todas as peças do jogo
+		var tamanhoDoNivel = tabela.obterTamanhoDoNivel(nivel);
+		NUM_DE_LINHAS = tamanhoDoNivel[0];
+		NUM_DE_COLUNAS = tamanhoDoNivel[1];
+		var i,j;
+		var vetorAuxiliarParaPegarTexto;
+		var vetorAuxiliarParaGerarMatrizAleatoria = new Array(NUM_DE_COLUNAS*NUM_DE_LINHAS);
+		var contador = 0;
+		var textosDoNivel = tabela.obterTexto(nivel);
+		var numeroDeGruposDoNivel = textosDoNivel.length;
+		for(i=0;i<numeroDeGruposDoNivel;i++)
+		{
+			vetorAuxiliarParaPegarTexto = textosDoNivel[i];			
+			for(j=0;j<vetorAuxiliarParaPegarTexto.length;j++)
+			{
+				vetorAuxiliarParaGerarMatrizAleatoria[contador] = {grupo:i, texto:vetorAuxiliarParaPegarTexto[j]};
+				contador++;				
+			}
+		}			
+		matrizDePecas = new Array(NUM_DE_COLUNAS);
+		for(i=0;i<NUM_DE_LINHAS;i++)
+		{
+			matrizDePecas[i] = new Array(NUM_DE_LINHAS);
+			for(j=0;j<NUM_DE_COLUNAS;j++)
+			{
+				var aux = Math.floor(Math.random()*vetorAuxiliarParaGerarMatrizAleatoria.length);
+				var aux2 = vetorAuxiliarParaGerarMatrizAleatoria.splice(aux,1)[0];
+				var grupo = aux2.grupo;
+				var texto = aux2.texto;
+				
+				matrizDePecas[i][j] = {
+					pos:	[i,j],
+					id:		i*NUM_DE_LINHAS+j,
+					valor:	grupo,
+					texto:	texto
+				};
+				
+				//Forçar a matriz
+				/*matrizDePecas[i][j] = {
+					pos:	[i,j],
+					id:		i*NUM_DE_LINHAS+j,
+					valor:	1,
+					texto:	1
+				};*/
+			}
+		}		
+	}	
+	function validarLigacao(peca1, peca2, trocarCaminho)
+	{
+		//Essa função é chamada para checar se as duas peças se ligam
+		if(peca1.valor == peca2.valor)
+		{
+			return checarConexao(peca1.pos, peca2.pos, trocarCaminho);			
+		}
+		else
+			return false;
+	}
+	function checarConexao(pos1, pos2, trocarCaminho)
+	{
+		return checagem.verificar(matrizDePecas, pos1, pos2, [NUM_DE_LINHAS, NUM_DE_COLUNAS], trocarCaminho);
+	}
+	function tratarPecasLigadas(peca1, peca2)
+	{		
+		//Tiramos as peças da matriz
+		matrizDePecas[peca1.pos[0]].splice(peca1.pos[1],1,null);
+		matrizDePecas[peca2.pos[0]].splice(peca2.pos[1],1,null);
+		
+		pontuacao += PONTOS_POR_ACERTO;
+		
+		//Se tiver gravidade nós a aplicamos
+		if(gravidadeLigada)
+		{
+			aplicarGravidade(peca1.pos);
+			aplicarGravidade(peca2.pos);
+		}
+		
+		if(checarFimPorVitoria())
+		{
+			if(nivel >= 2)
+			{
+				estadoDoJogo = 'finalizado';
+			}
+			else
+			{
+				estadoDoJogo = "venceu";
+			}
+			clearInterval(cronometro);
+			return true;
+		}
+		else if(checarFimPorImpossibilidade())
+		{
+			estadoDoJogo = "impossivel";
+			return true;
+		}
+	}
+	function aplicarGravidade(pos)
+	{
+		var j = pos[1];	
+		var acabou = false;
+		while(!acabou)
+		{
+			acabou = true;
+			for(i=pos[0];i>0;i--)
+			{
+				if(matrizDePecas[i][j] == null && matrizDePecas[i-1][j] != null)
+				{
+					acabou = false;
+					matrizDePecas[i][j] = matrizDePecas[i-1][j];
+					matrizDePecas[i][j].pos[0] += 1; //atualizamos a posição da peça na matriz
+					matrizDePecas[i-1][j] = null;
+				}
+			}
+			pos[1]--;
+		}				
+	}
+	function matrizVazia()
+	{
+		var i,j;
+		var vazia = true;
+		for(i=0;i<NUM_DE_COLUNAS;i++)
+			for(j=0;j<NUM_DE_LINHAS;j++)
+				if(matrizDePecas[i][j] != null)
+				{
+					vazia = false;
+					break;
+				}
+				
+		return vazia;
+	}
+	function resetarPontuacao()
+	{
+		if(nivel == 0)
+		{
+			pontuacao = 0;
+		}
+	}
+	function ajustarPontuacao(valor)
+	{
+		pontuacao += valor;
+	}
+	function resetarCronometro()
+	{
+		tempoRestante = 900; //15 minutos = 900 segundos
+	}
+	function atualizar()
+	{
+		ajustarCronometro();
+		if(checarFinalDeJogo())
+			clearInterval(cronometro);
+	}
+	function ajustarCronometro()
+	{
+		tempoRestante--;
+	}
+	function checarFinalDeJogo()
+	{
+		//Essa função checa se o jogo já acabou
+		//O jogo acaba quando não existem mais peças em jogo, não existem mais ligações ou o tempo acabou
+		if(checarFimPorTempo())
+		{
+			estadoDoJogo = "perdeu";
+			clearInterval(cronometro);
+			return true;
+		}else
+			return false;
+	}
+	function checarFimPorVitoria()
+	{
+		return matrizVazia();
+	}
+	function checarFimPorTempo()
+	{
+		return tempoRestante<=0;
+	}
+	function checarFimPorImpossibilidade()
+	{
+		return checarPossivelLigacao() == false;
+	}
+	function checarPossivelLigacao()
+	{
+		//algoritmo de checagem de combinação
+		var i,j,x,y;
+		for(i=0;i<NUM_DE_LINHAS;i++)
+		{
+			for(j=0;j<NUM_DE_COLUNAS;j++)
+			{
+				if(matrizDePecas[i][j] == null)
+					continue;
+				else
+				{
+					for(x=0;x<NUM_DE_LINHAS;x++)
+						for(y=0;y<NUM_DE_COLUNAS;y++)
+						{
+							if(matrizDePecas[x][y] == null || matrizDePecas[i][j] == matrizDePecas[x][y])
+								continue;
+							else
+							{
+								var peca1 = matrizDePecas[i][j];
+								var peca2 = matrizDePecas[x][y];								
+								if(validarLigacao(peca1, peca2, false))
+								{
+									return [peca1,peca2];
+								}
+								else
+									continue;
+							}
+						}
+				}
+			}
+		}
+		return false;
+	}
+	function reordenarMatriz()
+	{
+		var i,j;		
+		var vetorPos = new Array();
+		var vetorPecas = new Array();
+		var vetorMudancas = new Array();
+			
+		for(i=0;i<NUM_DE_COLUNAS;i++)
+			for(j=0;j<NUM_DE_LINHAS;j++)
+				if(matrizDePecas[i][j] != null)
+				{
+					vetorPos.push([i,j]);
+					vetorPecas.push(matrizDePecas[i].splice(j,1,null)[0]);
+				}
+				
+		
+		for(i=0;i<vetorPos.length;i++)
+		{
+			var x = vetorPos[i][0];
+			var y = vetorPos[i][1];
+			matrizDePecas[x][y] = vetorPecas.splice(Math.floor(Math.random()*vetorPecas.length),1)[0];
+			var mudanca = {x0:matrizDePecas[x][y].pos[0], y0:matrizDePecas[x][y].pos[1], x1:vetorPos[i][0], y1:vetorPos[i][1]};	
+			matrizDePecas[x][y].pos = vetorPos[i];
+			vetorMudancas.push(mudanca);
+		}
+
+		return vetorMudancas;
+	}
+	
+	
+	return		{	
+		checarFimPorImpossibilidade: function()
+		{
+			if(checarFimPorImpossibilidade())
+			{
+				estadoDoJogo = "impossivel";
+				return true;
+			}else
+			{
+				return false;
+			}
+		},	
+		iniciarJogo:function()
+		{	
+			if(estadoDoJogo == "perdeu")
+				estadoDoJogo = 'parado';
+			
+			//Ajusta cronometro, gera matriz, incia pontuação
+			if(estadoDoJogo == "parado")
+			{				
+				resetarCronometro();
+				resetarPontuacao();
+				gerarNovaMatriz();
+				estadoDoJogo = "jogando";
+				cronometro = setInterval(atualizar, 1000);				
+			}
+		},
+		proximoNivel:function()
+		{
+			clearInterval(cronometro);
+			estadoDoJogo = "parado";
+			
+			if(nivel<NIVEL_MAXIMO)
+			{
+				nivel++;
+			}
+			else
+			{
+				estadoDoJogo = "finalizado";
+			}
+		},
+		fazerPossivel:function()
+		{
+			if(estadoDoJogo == "impossivel")
+			{
+				estadoDoJogo = "jogando";
+				return reordenarMatriz();
+			}
+		},
+		sairJogo:function()
+		{			
+			estadoDoJogo = "parado";
+			nivel = 0;
+			clearInterval(cronometro);
+		},	
+		retornarPecasQueSeLigam: function ()
+		{			
+			if(pontuacao >= CUSTO_DA_DICA)
+			{
+				pontuacao -= CUSTO_DA_DICA;
+				return checarPossivelLigacao();
+			}
+			else
+			{
+				
+				return false;
+			}
+		},
+		checarPossivelLigacao: checarPossivelLigacao,
+		tentarLigar: function(pos1, pos2)
+		{
+			if(estadoDoJogo == 'jogando')
+			{
+				var peca1 = matrizDePecas[pos1[0]][pos1[1]];
+				var peca2 = matrizDePecas[pos2[0]][pos2[1]];
+				
+				if(validarLigacao(peca1, peca2, true))
+				{
+					tratarPecasLigadas(peca1, peca2);
+					return true;
+				}
+				else
+					return false;
+			}
+			else
+			{
+				return false;
+			}
+		},
+
+		obterTempoRestante:function()
+		{
+			return tempoRestante;
+		},
+		obterCaminho:function()
+		{
+			return checagem.obterCaminho();
+		},
+		obterPontuacao:function()
+		{
+			return pontuacao;
+		},
+		obterMatrizDePecas:function()
+		{			
+			return matrizDePecas;
+		},
+		obterEstadoDeJogo:function()
+		{
+			return estadoDoJogo;
+		},
+		obterNumeroDeLinhas: function()
+		{
+			return NUM_DE_LINHAS;
+		},
+		obterNumeroDeColunas: function()
+		{
+			return NUM_DE_COLUNAS;
+		},
+		obterCustoDaDica: function() {
+			return CUSTO_DA_DICA;
+		},
+		ajustarGravidade:function(valor)
+		{
+			if(estadoDoJogo == "parado")
+				gravidadeLigada = valor;
+		},	
+		obterGravidade: function()
+		{
+			return gravidadeLigada;
+		},
+		tratarFinalDoJogo: function()
+		{
+			nivel = 0;
+			estadoDoJogo = 'parado';
+		},
+		pausarCronometro: function() {
+			estadoDoJogo = 'pausado';
+			clearInterval(cronometro);
+		},
+		continuarCronometro: function() {
+			estadoDoJogo = 'jogando';
+			cronometro = setInterval(atualizar, 1000);	
+		},
+	};
+});
---
+++ MathJong/scripts/tutorial/tutorial.js
@@ -0,0 +1,16 @@
+
+define({
+	tutorialAtual: null,
+
+	 texto: [
+	 	"Bem vindo ao math-jong.",
+		"O objetivo é remover todas as peças do tabuleiro formando pares.",
+		"Os pares devem ser de mesmo valor cada peça.",
+		"Você precisa encontrar pares em uma distância de no máximo três ângulos.",
+		"Caso precise de ajuda clique na DICA.",
+		"Se precisar relembrar alguns conteúdos clique em Lembrete.",
+		"Você tem 15 minutos para realizar esta etapa, mostrado no cronometro.",
+		"Caso queira retirar o som, clique na nota musical.",
+		"Boa Sorte !!!",
+	]
+});
