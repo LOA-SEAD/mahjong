9723c429a2c6274cf33a1234c3c4df0fea567e6a
Lucas David <lucasdavid@drexel.edu>
2014-08-09 10:47:59 -0300

    fix #1

--- MathJong/scripts/tutorial/tutorial.js
+++ MathJong/scripts/tutorial/tutorial.js
@@ -6,7 +6,7 @@ define({
 	 	"Bem vindo ao math-jong.",
 		"O objetivo é remover todas as peças do tabuleiro formando pares.",
 		"Os pares devem ser de mesmo valor cada peça.",
-		"Você precisa encontrar pares em uma distância de no máximo três ângulos.",
+		"Você precisa encontrar pares em uma distância de no máximo três retas.",
 		"Caso precise de ajuda clique na DICA.",
 		"Se precisar relembrar alguns conteúdos clique em Lembrete.",
 		"Você tem 15 minutos para realizar esta etapa, mostrado no cronometro.",
